94a0dd016275286add380c40893ba05b89e5f468
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-27 14:40:53 +0530

    input validation fix

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -17,13 +17,18 @@ class Todo extends Component {
 
   handleAddItem(e) {
     e.preventDefault();
-    let id = new Date().getMilliseconds();
-    this.props.addItem({ id: id, name: this.state.item });
-    this.setState({ item: "" });
+
+    if (this.state.item == "") {
+      alert("Please add item list");
+    } else {
+      let id = new Date().getMilliseconds();
+      this.props.addItem({ id: id, name: this.state.item });
+      this.setState({ item: "" });
+    }
   }
   render() {
     const { item } = this.state;
-    const {todoList} = this.props
+    const { todoList } = this.props;
 
     return (
       <div>
@@ -40,11 +45,9 @@ class Todo extends Component {
 
         {/* DISPLAY ITEM LIST */}
 
-        {
-            todoList.map((e)=>{
-                return <Card key={e.id} todoList={e}/>
-            })
-        }
+        {todoList.map((e) => {
+          return <Card key={e.id} todoList={e} />;
+        })}
       </div>
     );
   }
